0d6002fbf53995b8f00435fd451ae3b438bec4d8
Manaya20 <pachpor.manaya@gmail.com>
2023-07-29 17:30:20 +0530

    ✨ add funtions

--- main.py
+++ main.py
@@ -4,31 +4,70 @@ from tkinter import *
 from tkinter.ttk import *
 import numpy as np
 import sys
-df=pd.read_csv('/Users/manayapachpor/Downloads/BLR_DEPARTURE.csv', encoding='unicode_escape')
+df=pd.read_csv('/Users/manayapachpor/Downloads/airradar/BLR_DEPARTURE.csv', encoding='unicode_escape')
 MainWindow = Tk()
 MainWindow.geometry("800x400")
 MainWindow.title('AIRADAR')
-MainWindow.configure(bg='White')
+MainWindow.configure(bg='black')
 
 def display_src():
     newWindow = Toplevel(MainWindow)
-    newWindow.title("SOURCE")
-    newWindow.geometry("900x600")
-    #newWindow.configure(bg='black')
-   
-    disp = tkinter.Label(newWindow, text="Enter Flight Number ")
+    newWindow.title("Display Source")
+    newWindow.geometry("2000x2000")
+    newWindow.configure(bg='black')
+    var=tkinter.StringVar()
+    def get_1():
+        model=var.get()
+        disp_1 = df.loc[(df['Flight_No']==model),['Flight_No','Airlines_Name','Arrival_City_Code']]
+        txt = Text(newWindow)
+        txt.pack()
+        class PrintToTXT(object):
+            def write(self, s):
+                txt.insert(END, s)
+        sys.stdout = PrintToTXT()
+        print(disp_1)
+    disp = tkinter.Label(newWindow,bg='black',fg='white', text="Enter Flight Number ", font=('bold', 20))
     disp.place(x=20, y=30)
-    
-    b_get_1=tkinter.Button(newWindow, text="RUN",bg='black')
-    b_get_1.configure(bg='black')
-    b_get_1.place(x=160, y=100)
+    disp = Entry(newWindow, textvariable=var)
+    disp.place(x=220, y=30)
+    b_get_1=Button(newWindow, text="RUN",command=get_1)
+    b_get_1.place(x=200, y=100)
 
-dispBtn=tkinter.Button(MainWindow,text="FLIGHT SOURCE",bg='black',command=display_src)
+def terminal():
+    newWindow = Toplevel(MainWindow)
+    newWindow.title("Display Flight with Specified Terminal")
+    newWindow.geometry("2000x2000")
+    newWindow.configure(bg='black')
+    var = tkinter.StringVar()
+    def get_1():
+        model=var.get()
+        disp_1 = df.loc[(df['Arrival_terminal']==model),['Flight_No','Airlines_Name','Arrival_Airport']]
+        txt = Text(newWindow)
+        txt.pack()
+        class PrintToTXT(object):
+            def write(self, s):
+                txt.insert(END, s)
+        sys.stdout = PrintToTXT()
+        print(disp_1)
+    disp = tkinter.Label(newWindow,bg='black',fg='white', text="Enter Terminal Number ", font=('bold', 20))
+    disp.place(x=20, y=30)
+    disp = tkinter.Entry(newWindow, bg='black',fg='white',textvariable=var)
+    disp.place(x=300, y=30)
+    b_get_1=Button(newWindow, text="RUN",command=get_1)
+    b_get_1.place(x=200, y=100)
+
+
+def exitWindow():
+    sys.exit()
+
+dispBtn=tkinter.Button(MainWindow,text="DISPLAY FLIGHT SOURCE",command=display_src,bg='black')
 dispBtn.configure(bg='black')
 dispBtn.place(x=23, y=50)
-lb=tkinter.Label(MainWindow, text="Enter flight number",fg='white')
-lb.place(x=23,y=39)
-lb.pack()
 
+terBtn=tkinter.Button(MainWindow, bg='black',text="DISPLAY TERMINAL", command=terminal)
+terBtn.place(x=320, y=50)
+
+extBtn=tkinter.Button(MainWindow, bg='black',text="Exit", command=exitWindow)
+extBtn.place(x=480, y=210)
 
 MainWindow.mainloop()
