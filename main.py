6b5bc0b2b90c3f03df4c206d5008a091ab027219
Manaya20 <pachpor.manaya@gmail.com>
2023-07-30 12:03:58 +0530

    ⚡️ create functions for buttons

--- main.py
+++ main.py
@@ -93,6 +93,131 @@ def status():
     var = tkinter.StringVar()
     var2 = tkinter.StringVar()
     var3=tkinter.StringVar()
+    def get_1():
+        model = var.get()
+        city_code = var2.get()
+        date=var3.get()
+        disp_1 = df.loc[(df['Flight_No']==model)&(df['Arrival_City_Code']==city_code)&(df['Date_wise']==date),['Date_wise','Flight_No','Airlines_Name','Flight_Time_status']]
+        txt = Text(newWindow)
+        txt.pack()
+        class PrintToTXT(object):
+            def write(self, s):
+                txt.insert(END, s)
+        sys.stdout = PrintToTXT()
+        print(disp_1)
+    disp = tkinter.Label(newWindow, bg='black',fg='white',text="Enter Flight Number ", font=('bold', 20))
+    disp.place(x=20, y=30)
+    disp = tkinter.Entry(newWindow, bg='black',fg='white',textvariable=var)
+    disp.place(x=300, y=30)
+    disp2 = tkinter.Label(newWindow, bg='black',fg='white',text="Arrival City Code ", font=('bold', 20))
+    disp2.place(x=20, y=60)
+    disp2 = tkinter.Entry(newWindow, bg='black',fg='white',textvariable=var2)
+    disp2.place(x=300, y=60)
+    disp3 = tkinter.Label(newWindow, bg='black',fg='white',text="Date ", font=('bold', 20))
+    disp3.place(x=20, y=90)
+    disp3 = tkinter.Entry(newWindow, bg='black',fg='white',textvariable=var3)
+    disp3.place(x=300, y=90)
+    b_get_1 = tkinter.Button(newWindow, text="RUN", command=get_1)
+    b_get_1.place(x=200, y=200)
+
+def aircraft():
+    newWindow = Toplevel(MainWindow)
+    newWindow.title("Aircraft Details")
+    newWindow.geometry("2000x2000")
+    newWindow.configure(bg='black')
+    var = tkinter.StringVar()
+    var2 = tkinter.StringVar()
+    def get_1():
+        model = var.get()
+        city_code = var2.get()
+        disp_1 = df.loc[((df['Flight_No'] == model) & (df['Arrival_City_Code'] == city_code)), ['Flight_No','Airlines_Name','Aircraft_equipment_description']]
+        txt = Text(newWindow)
+        txt.pack()
+        class PrintToTXT(object):
+            def write(self, s):
+                txt.insert(END, s)
+        sys.stdout = PrintToTXT()
+        print(disp_1)
+    disp = tkinter.Label(newWindow, bg='black',fg='white',text="Enter Flight Number ", font=('bold', 20))
+    disp.place(x=20, y=30)
+    disp = tkinter.Entry(newWindow, bg='black',fg='white',textvariable=var)
+    disp.place(x=300, y=30)
+    disp2 = tkinter.Label(newWindow, bg='black',fg='white',text="Enter Arrival City Code ", font=('bold', 20))
+    disp2.place(x=20, y=60)
+    disp2 = tkinter.Entry(newWindow, bg='black',fg='white',textvariable=var2)
+    disp2.place(x=300, y=60)
+    b_get_1 = Button(newWindow, text="RUN", command=get_1)
+    b_get_1.place(x=200, y=100)
+
+def delay():
+    newWindow = Toplevel(MainWindow)
+    newWindow.title("Flight Delay Details")
+    newWindow.geometry("2000x2000")
+    newWindow.configure(bg='black')
+    var = tkinter.StringVar()
+    var2 = tkinter.StringVar()
+    var3 = tkinter.StringVar()
+    def get_1():
+        model = var.get()
+        status = var2.get()
+        date = var3.get()
+        disp_1 = df.loc[(df['Flight_No'] == model) & (df['Flight_Time_status'] == status)& (df['Date_wise'] == date), ['Flight_No', 'Airlines_Name','Flight_delay_by_time','Arrivial_time_estimated']]
+        txt = Text(newWindow)
+        txt.pack()
+
+        class PrintToTXT(object):
+            def write(self, s):
+                txt.insert(END, s)
+
+        sys.stdout = PrintToTXT()
+        print(disp_1)
+
+    disp = tkinter.Label(newWindow, bg='black',fg='white',text="Enter Flight Number ", font=('bold', 20))
+    disp.place(x=20, y=30)
+    disp = tkinter.Entry(newWindow, bg='black',fg='white',textvariable=var)
+    disp.place(x=300, y=30)
+    disp2 = tkinter.Label(newWindow, text="Status ", bg='black',fg='white',font=('bold', 20))
+    disp2.place(x=20, y=60)
+    disp2 = tkinter.Entry(newWindow, bg='black',fg='white',textvariable=var2)
+    disp2.place(x=300, y=60)
+    disp3 = tkinter.Label(newWindow, text="Date ", bg='black',fg='white',font=('bold', 20))
+    disp3.place(x=20, y=90)
+    disp3 = tkinter.Entry(newWindow, bg='black',fg='white',textvariable=var3)
+    disp3.place(x=300, y=90)
+    b_get_1 = Button(newWindow, text="RUN", command=get_1)
+    b_get_1.place(x=200, y=200)
+    
+def detail():
+    newWindow = Toplevel(MainWindow)
+    newWindow.title("Flight Details")
+    newWindow.geometry("2000x2000")
+    newWindow.configure(bg='black')
+    var = tkinter.StringVar()
+    var2 = tkinter.StringVar()
+    def get_1():
+        city = var.get()
+        date = var2.get()
+        disp_1 = df.loc[((df['Arrival_City'] == city)&(df['Date_wise']==date)),['Date_wise','Flight_No', 'Airlines_Name','Departure_Date','Departure_time_estimated','Arrival_Date','Arrivial_time_estimated']]
+        txt = Text(newWindow)
+        txt.pack()
+
+        class PrintToTXT(object):
+            def write(self, s):
+                txt.insert(END, s)
+
+        sys.stdout = PrintToTXT()
+        print(disp_1)
+
+    disp = tkinter.Label(newWindow, bg='black',fg='white',text="Enter City ", font=('bold', 20))
+    disp.place(x=20, y=30)
+    disp = tkinter.Entry(newWindow, bg='black',fg='white',textvariable=var)
+    disp.place(x=300, y=30)
+    disp2 = tkinter.Label(newWindow, bg='black',fg='white',text="Enter Date ", font=('bold', 20))
+    disp2.place(x=20, y=60)
+    disp2 = tkinter.Entry(newWindow, bg='black',fg='white',textvariable=var2)
+    disp2.place(x=300, y=60)
+    b_get_1 = Button(newWindow, text="RUN", command=get_1)
+    b_get_1.place(x=200, y=100)
 
 
 def exitWindow():
@@ -108,8 +233,22 @@ terBtn.place(x=320, y=50)
 distBtn=tkinter.Button(MainWindow, bg='black',text="DISPLAY DISTANCE", command=distance)
 distBtn.place(x=600, y=50)
 
+statBtn=tkinter.Button(MainWindow, bg='black',text="DISPLAY FLIGHT STATUS",  command=status)
+statBtn.place(x=70, y=130)
+
+airBtn=tkinter.Button(MainWindow,  bg='black',text="DISPLAY AIRCRAFT DETAILS",  command=aircraft)
+airBtn.place(x=290, y=130)
+
+delayBtn=tkinter.Button(MainWindow,  bg='black',text="DISPLAY DELAY DETAILS",  command=delay)
+delayBtn.place(x=560, y=130)
+
+detailBtn=tkinter.Button(MainWindow,bg='black', text="DISPLAY FLIGHT DETAILS",  command=detail)
+detailBtn.place(x=200, y=210)
+
+
 extBtn=tkinter.Button(MainWindow, bg='black',text="Exit", command=exitWindow)
 extBtn.place(x=480, y=210)
 
 MainWindow.mainloop()
+
 
